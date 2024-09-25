cb50a0f3bbca90d712b2f7f0c416b47bef2e635c
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-09-25 19:12:44 +0100

    edit2

---
+++ les boucle 01/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter an number between 1&10\n");
+    scanf("%d",&b);
+    for (a=0;a<=10;a++){
+
+
+    printf("%d x %d = %d\n",a,b,a*b);
+    }
+
+
+
+
+    return 0;
+}
